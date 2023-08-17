e16e3cd960cf749ae5739506887a271c9809599f
afkshreshtha <shreshtha1345@gmail.com>
2023-08-17 10:31:21 +0530

    error v5

--- app/sign-in/page.jsx
+++ app/sign-in/page.jsx
@@ -1,33 +1,18 @@
-'use client'
-
+"use client"
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
-
 import { supabase } from '../utils/supabase'
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 
 const Login = () => {
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(false)
-
   const router = useRouter()
-
-  useEffect(() => {
-    supabase.auth.onAuthStateChange((event, session) => {
-      if (event === 'SIGNED_IN') {
-        // Reload the page and navigate to '/'
-        // window.location.reload();
-        router.push('/')
-      }
-    })
-  }, [router])
 
   useEffect(() => {
     const fetchSession = async () => {
       const session = await supabase.auth.getSession()
-      if (session?.data.session === null) {
-        setIsUserLoggedIn(true)
-      }
+      setIsUserLoggedIn(!session)
     }
     fetchSession()
   }, [])
@@ -39,10 +24,10 @@ const Login = () => {
       router.push('/')
     }
   }, [isUserLoggedIn, router])
+
   return (
     <div className="">
-      <Auth
-        onlyThirdPartyProviders={true}
+      <Auth  
         supabaseClient={supabase}
         appearance={{ theme: ThemeSupa }}
         theme="dark"
